4c8713abe88bc0e0e73f3b30af05d954b5cfdea8
jungy24 <jungy24@gmail.com>
2018-11-15 02:55:12 -0500

    Initial Commit

---
+++ 2018Tumblr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2018Tumblr
+//
+//  Created by Jungyoon Yu on 11/15/18.
+//  Copyright © 2018 JungyoonYu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
